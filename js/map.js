4c69f4fb663ea5bf8d36165e1f82ef2a4a0a042f
Monsieur19 <42003367+Monsieur19@users.noreply.github.com>
2018-11-29 21:45:53 +0300

    Revert "2.1"
    This reverts commit b803a81a357b0f4b12bc0a88f3bdc077e296e526.

--- js/map.js
+++ js/map.js
@@ -65,6 +65,7 @@ var createCustomer = function () {
       y: pointY
     }
   };
+  obj.offer.address = obj.location.x + ', ' + obj.location.y;
   return obj;
 };
 var getAmountCustomers = function (count) {
