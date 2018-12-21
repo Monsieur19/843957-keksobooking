dfb35394de99b32cb139e34825088cca49c0a885
Monsieur19 <42003367+Monsieur19@users.noreply.github.com>
2018-12-21 23:37:10 +0300

    Revert "пк 55555"
    This reverts commit 8bc1cbf1a1ba3e4af23cf2e7d2940848466f73e8.

--- js/pin.js
+++ js/pin.js
@@ -13,23 +13,23 @@
     }
   };
   var renderPin = function (arr) {
+    window.card.delete();
+    var templatePin = document.querySelector('#pin').content.querySelector('.map__pin');
+    var fragmentPin = document.createDocumentFragment();
+    var customerElement = templatePin.cloneNode(true);
+    customerElement.style.left = arr.location.x + 'px';
+    customerElement.style.top = arr.location.y + 'px';
+    customerElement.querySelector('img').src = arr.author.avatar;
+    customerElement.title = arr.offer.title;
     if (arr.offer) {
-      window.card.delete();
-      var templatePin = document.querySelector('#pin').content.querySelector('.map__pin');
-      var fragmentPin = document.createDocumentFragment();
-      var customerElement = templatePin.cloneNode(true);
-      customerElement.style.left = arr.location.x + 'px';
-      customerElement.style.top = arr.location.y + 'px';
-      customerElement.querySelector('img').src = arr.author.avatar;
-      customerElement.title = arr.offer.title;
       fragmentPin.appendChild(customerElement);
-      customerElement.addEventListener('click', function () {
-        deleteActive();
-        customerElement.classList.add('map__pin--active');
-        window.card.open(arr);
-      });
-      document.querySelector('.map__pins').appendChild(fragmentPin);
     }
+    customerElement.addEventListener('click', function () {
+      deleteActive();
+      customerElement.classList.add('map__pin--active');
+      window.card.open(arr);
+    });
+    document.querySelector('.map__pins').appendChild(fragmentPin);
   };
   window.pin = {
     render: renderPin,
